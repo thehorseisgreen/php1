5fbef29fd066e42bf20298f7bb819fb9b9cd28cf
thehorseisgreen <kookv30122548@gmail.com>
2025-11-20 09:46:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo "Hellow world !!!";
+    echo "br";
+    echo "<h1>My Name is Jitsupa.</h2>";
+    ?>
+</body>
+</html>
